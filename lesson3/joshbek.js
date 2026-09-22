f05743a7b1e9c2ce230c6c5deedc366badc5c08a
Your Name <you@example.com>
2026-09-22 15:55:13 +0600

    first commit

---
+++ lesson3/joshbek.js
@@ -0,0 +1,49 @@
+// let email = prompt("Ведите ваш email");
+// let password = prompt("Ведите ваш пароль")
+// let userEmail = "admin"
+// let userPassword = "admin123"
+
+// if(email === userEmail && password === userPassword){
+//     alert("Вы успешно вошли в систему")
+// }else {
+//     alert("неверный пароль или логин!")
+// }
+
+
+// let numer = prompt("Проверьте отрицательное оно или нет? ")
+
+// if("numer "){
+//     alert("НЕ ОТРИЦВАТЕЛЬНОЕ")
+
+
+
+let month = prompt ("ведите любой месяц");
+switch(month){
+    case "январь":
+    case "февраль":
+    case "март":
+        alert ("зима");
+
+
+    case "апрель":
+    case "май":
+    case "март":
+        alert ("весна");
+
+
+    case "июнь":
+    case "июль":
+    case "август":
+        alert ("Лето");
+
+    case "сентябрь":
+    case "октябрь":
+    case "ноябрь":
+        alert ("осень");
+
+
+
+
+
+
+}
